3d970c9d2ceef588a25133cc3655d86e6b0c7b17
Adelaide <adelaide.craig@outlook.com>
2022-06-09 13:48:54 -0500

    buggie board

--- tic-tac-toe/src/components/BoardGame.vue
+++ tic-tac-toe/src/components/BoardGame.vue
@@ -2,13 +2,44 @@
 <div class="container">
   <h2 v-if="winner">Winner: {{ winner }} </h2>
   <h2 v-else> Players Move: {{ player }} </h2>
-  <button @click=""
+  <button @click="reset" class="btn">Press here to reset</button>
+<div v-for="x in 3" :key="x" class="row">
+
+  <button v-for="y in 3" :key="y" class="square" @click="move(x, y)">
+    {{ squares[x][y] }}
+  </button>
+   </div>
+   <h2 class="mt-5">History</h2>
+   <div v-for="(game, idx) in history" :key="idx">
+     Game {{ idx + 1 }}: {{ game }} won
+   </div>
 </div>
 
 </template>
 
 <script>
-import { computed, ref } from 'vue'
+
+import { computed, onMounted, ref, watch } from 'vue'
+
+const calcaulateWinner = squares => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[1]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a]
+    }
+  }
+  return null
+}
   export default{
     setup() {
       const player = ref('X')
@@ -17,11 +48,11 @@ import { computed, ref } from 'vue'
         ['', '', ''],
         ['', '', '']
       ])
-      const winner = computed(() => calcaulateWinner(sqaures.values.flat()))
+      const winner = computed(() => calcaulateWinner(squares.value.flat()))
 
       const move = (x, y) => {
         if(winner.value) return
-        sqaures.value[x][y] = player.value
+        squares.value[x][y] = player.value
         player.value = player.value === 'O' ? 'X' : 'O'
       }
 
@@ -34,7 +65,18 @@ import { computed, ref } from 'vue'
       ]
         
       }
-  
+      const history = ref([])
+      watch(winner, (current, previous) => {
+        if(current && !previous) {
+          history.value.push(current)
+          localStorage.setItem('history', JSON.stringify(history.value))
+        }
+      })
+
+      onMounted(() => {
+        history.value = JSON.parse(localStorage.getItem('history')) ?? []
+      })
+    return { winner, player, squares, move, reset, history}
     }
   }
 </script>
